bc5917eee399c0b74b37308f2f12e5123df707b4
Mubasshir Ahmed <lastmubasshir@gmail.com>
2023-06-10 10:33:05 +0600

    partnership section added

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Hero from './components/Hero.vue';
 import Navbar from './components/Navbar.vue';
+import Partnership from './components/Partnership.vue';
 </script>
 
 <template>
   <div class="overflow-x-hidden">
     <Navbar />
     <Hero />
+    <Partnership />
   </div>
 </template>
---
+++ src/components/Partnership.vue
@@ -0,0 +1,87 @@
+<template>
+  <section id="partnership" class="py-10">
+    <div class="flex flex-col gap-5 items-center">
+      <h1 class="text-4xl font-bold tracking-tighter py-7">
+        Global Partnership Reach
+      </h1>
+      <p class="text-2xl text-gray-500 font-thin pb-7">
+        With a promise of spreading the world's most advanced technologies and
+        services
+      </p>
+    </div>
+    <div class="grid grid-cols-6 gap-6 mx-8 rounded-2xl pt-7">
+      <div class="relative">
+        <img
+          src="../assets/netherlands.jpg"
+          alt=""
+          class="rounded-md hover:translate-y-3 transition-all duration-300"
+        />
+        <div
+          class="absolute flex items-center bottom-20 left-0 bg-primary-200 w-32 h-12 rounded-r-md p-2 text-white tracking-tighter text-center"
+        >
+          Netherlands
+        </div>
+      </div>
+      <div class="relative">
+        <img
+          src="../assets/uk.jpg"
+          alt=""
+          class="rounded-md hover:translate-y-3 transition-all duration-300"
+        />
+        <div
+          class="absolute flex items-center justify-center bottom-20 left-0 bg-primary-200 w-32 h-12 rounded-r-md p-2 text-white tracking-tighter text-center"
+        >
+          UK
+        </div>
+      </div>
+      <div class="relative">
+        <img
+          src="../assets/usa.jpg"
+          alt=""
+          class="rounded-md hover:translate-y-3 transition-all duration-300"
+        />
+        <div
+          class="absolute flex items-center bottom-20 left-0 bg-primary-200 w-32 h-12 rounded-r-md p-2 text-white tracking-tighter text-center"
+        >
+          United States
+        </div>
+      </div>
+      <div class="relative">
+        <img
+          src="../assets/netherlands.jpg"
+          alt=""
+          class="rounded-md hover:translate-y-3 transition-all duration-300"
+        />
+        <div
+          class="absolute flex items-center bottom-20 left-0 bg-primary-200 w-32 h-12 rounded-r-md p-2 text-white tracking-tighter text-center"
+        >
+          New Zealand
+        </div>
+      </div>
+      <div class="relative">
+        <img
+          src="../assets/australia.jpg"
+          alt=""
+          class="rounded-md hover:translate-y-3 transition-all duration-300"
+        />
+        <div
+          class="absolute flex items-center bottom-20 left-0 bg-primary-200 w-32 h-12 rounded-r-md p-2 text-white tracking-tighter text-center"
+        >
+          Australia
+        </div>
+      </div>
+      <div class="relative">
+        <img
+          src="../assets/malaysia.jpg"
+          alt=""
+          class="rounded-md hover:translate-y-3 transition-all duration-300"
+        />
+        <div
+          class="absolute flex items-center bottom-20 left-0 bg-primary-200 w-32 h-12 rounded-r-md p-2 text-white tracking-tighter text-center"
+        >
+          Malaysia
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
